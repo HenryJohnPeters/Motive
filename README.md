501210ac32ed9e51fcbb48b44d7933f471ce09e3
HenryJohnPeters <henryjohnpeters@gmail.com>
2022-09-08 22:54:26 +0100

    globe projection added

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
 
+name: Admin
+Mongo pw : Cnv2WWHVdoBSO9YU
+
 ## Getting Started
 
 First, run the development server:
--- components/Map/map.js
+++ components/Map/map.js
@@ -1,46 +1,83 @@
 import { useState, useRef, useEffect } from "react";
 import mapboxgl from "mapbox-gl"; // eslint-disable-line import/no-webpack-loader-syntax
-mapboxgl.accessToken = "pk.eyJ1IjoibWFwbWFrZXIxMjM0IiwiYSI6ImNraGM0aWllOTBhdnQycm5nMzk3ZzFwMW0ifQ.DUTM9-gpBSGHav22OeLhQg";
+mapboxgl.accessToken =
+  "pk.eyJ1IjoibWFwbWFrZXIxMjM0IiwiYSI6ImNraGM0aWllOTBhdnQycm5nMzk3ZzFwMW0ifQ.DUTM9-gpBSGHav22OeLhQg";
 export const Map = () => {
-	const mapContainer = useRef(null);
-	const map = useRef(null);
-	const [lng, setLng] = useState(-70.9);
-	const [lat, setLat] = useState(42.35);
-	const [zoom, setZoom] = useState(10);
-	const [mapObject, setMap] = useState(null);
+  const mapContainer = useRef(null);
+  const map = useRef(null);
+  const [lng, setLng] = useState(-70.9);
+  const [lat, setLat] = useState(42.35);
+  const [zoom, setZoom] = useState(11);
+  const [mapObject, setMap] = useState(null);
 
-	useEffect(() => {
-		if (map.current) return; // initialize map only once
-		map.current = new mapboxgl.Map({
-			container: mapContainer.current,
-			style: "mapbox://styles/mapbox/streets-v11",
-			center: [lng, lat],
-			zoom: zoom,
-		});
-		setMap(map);
-	}, []);
+  const [globeView, setGlobeView] = useState(false);
 
-	useEffect(() => {
-		if (navigator.geolocation) {
-			navigator.geolocation.getCurrentPosition(showPosition);
-		} else {
-			console.log("location not supported in this browser");
-		}
-	}, [mapObject]);
+  useEffect(() => {
+    if (map.current) return; // initialize map only once
+    map.current = new mapboxgl.Map({
+      container: mapContainer.current,
+      style: "mapbox://styles/mapbox/streets-v11",
+      center: [lng, lat],
+      zoom: zoom,
+      //dont nnedd
+      pitch: 50,
+      //bearing: 170,
+    });
 
-	const showPosition = (position) => {
-		console.log(position);
-		setLat(position.coords.latitude);
-		setLng(position.coords.longitude);
-		let coords = [lng, lat];
-		if (mapObject) {
-			mapObject.current.setCenter(coords);
-		}
-	};
+    getUser();
+    setMap(map);
+  }, [globeView]);
 
-	return (
-		<div>
-			<div ref={mapContainer} className="map-container" />
-		</div>
-	);
+  const enableGlobeMode = () => {
+    debugger;
+    mapObject.current.remove();
+    mapObject.current = new mapboxgl.Map({
+      projection: "globe",
+      container: mapContainer.current,
+      style: "mapbox://styles/mapbox/streets-v11",
+    });
+    setMap(mapObject);
+    debugger;
+  };
+
+  useEffect(() => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(showPosition);
+    } else {
+      console.log("location not supported in this browser");
+    }
+  }, [mapObject]);
+
+  const getUser = () => {
+    fetch("/api/hello")
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+      });
+  };
+
+  const showPosition = (position) => {
+    console.log(position);
+    setLat(position.coords.latitude);
+    setLng(position.coords.longitude);
+    let coords = [lng, lat];
+    if (mapObject) {
+      mapObject.current.setCenter(coords);
+    }
+  };
+
+  return (
+    <div className="w-full flex">
+      <div
+        onClick={() => enableGlobeMode()}
+        className="bg-white cursor-pointer rounded p-2 top-0 right-0 z-40 absolute"
+      >
+        Click here
+      </div>
+      <div className="bg-white rounded p-2 bottom-0 right-0 z-40 absolute">
+        Longitude: {lng} | Latitude: {lat} | Zoom: {zoom}
+      </div>
+      <div ref={mapContainer} className="map-container" />
+    </div>
+  );
 };
